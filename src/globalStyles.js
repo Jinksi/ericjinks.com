ed7f3be69d0a73338dd7fb710ddfec08a9592026
Jinksi <ericjinks@gmail.com>
2017-05-15 15:41:44 +1000

    Gift

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -62,6 +62,29 @@ export default () => injectGlobal`
     margin-bottom: 0.5em;
   }
 
+  img {
+    width: 100%;
+    height: auto;
+    mix-blend-mode: lighten;
+  }
+
+  .embed-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    height: 0;
+    overflow: hidden;
+    max-width: 100%;
+
+    iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      mix-blend-mode: lighten;
+    }
+  }
+
   ::selection{
     background: ${color.primary};
     opacity: 1;
