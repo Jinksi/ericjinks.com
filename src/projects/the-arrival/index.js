553b256beca555ebeaa642a4a66ae0d56a22c8fa
Jinksi <ericjinks@gmail.com>
2017-05-16 20:59:19 +1000

    The Arrival Description

--- src/projects/the-arrival/index.js
+++ src/projects/the-arrival/index.js
@@ -4,18 +4,24 @@ import arrival3 from './arrival-3.jpg'
 import arrival4 from './arrival-4.jpg'
 
 const content = `
-I collaborated with Brisbane band [Caligula's Horse](http://caligulashorse.com/) as visual artist / projectionist for their film clip 'A Gift To Afterthought'. The visuals were created with After Effects & Cinema 4D.
+This is short animation featuring artwork from the third chapter of Shaun Tan's graphic novel, The Arrival - shauntan.net
 
-![The Arrival – Shaun Tan](${arrival1})
-![The Arrival – Shaun Tan](${arrival2})
-![The Arrival – Shaun Tan](${arrival3})
-![The Arrival – Shaun Tan](${arrival4})
+I have created sound and music for the book, hoping to add movement and depth to the images. I aimed to create a soundtrack that is reflective of the book's visual style and shows the world from the character's point of view.
+
+The book tells a story of a man who leaves his wife and child in an impoverished town, seeking better prospects in an unknown country on the other side of a vast ocean. He eventually finds himself in a bewildering city of foreign customs, peculiar animals, curious floating objects and indecipherable languages.
+
+Sound used is both my own field recording and foley work as well as sound libraries from [freesound.org](http://freesound.org), [stephanschutze.com](http://stephanschutze.com) and [hissandaroar.com](http://hissandaroar.com).
 
 <p>
   <div class='embed-container'>
     <iframe src="https://player.vimeo.com/video/36180836" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
   </div>
 </p>
+
+![The Arrival – Shaun Tan](${arrival2})
+![The Arrival – Shaun Tan](${arrival1})
+![The Arrival – Shaun Tan](${arrival3})
+![The Arrival – Shaun Tan](${arrival4})
 `
 
 export default {
