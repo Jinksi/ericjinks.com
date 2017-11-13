886ceda8d85d718b3e12caaff565432a4548ea0a
Jinksi <ericjinks@gmail.com>
2017-11-14 08:39:02 +1000

    Update <Home /> content

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,30 +1,44 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Page from '../components/Page'
-import { Title, Section, Container, Flex, Col, H1 } from '../components/common'
+import { Link } from 'react-router-dom'
 
-export default (props) => (
+import Page from '../components/Page'
+import { Title, Section, Container, Flex, Col } from '../components/common'
+
+export default props => (
   <Page>
     <Section thin>
-      <Container>
+      <Container skinny>
         <Flex alignCenter>
           <Col>
             <Title>
               <div className='background animate-translate animate-translate-mobile' />
               <span>Eric Jinks</span>
             </Title>
+            <p>I am a web developer from the Gold Coast, Australia.</p>
             <p>
-              Creative developer, sound designer / musician.
+              My professional focus is on creating modern, performant websites
+              &amp; apps using JavaScript and Serverless architecture.
             </p>
             <p>
-              I mess with JavaScript, WebGL, WebVR, WebAudio to create audio and visual art / things.
+              You'll also find me experimenting with sound design, music,
+              JavaScript, WebGL, WebVR and WebAudio.
             </p>
+            <p>
+              If you'd like to get in touch, head over to the{' '}
+              <Link to='/contact'>Contact</Link> page or send me an email at{' '}
+              <a target='_blank' href='mailto:ericjinks@gmail.com'>
+                ericjinks@gmail.com
+              </a>
+            </p>
+            <h3>Interests</h3>
+            - JavaScript - React - Node - GraphQL - REST - HTML - CSS -
+            Serverless architecture - Progressive Web Apps - Headless CMS -
+            Service Workers - WebVR - WebAudio
           </Col>
         </Flex>
       </Container>
     </Section>
-    <Helmet
-      title='About'
-    />
+    <Helmet title='About' />
   </Page>
 )
