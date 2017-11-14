57cc6993756cde3ed7e431d405843e3cd7513ea2
Jinksi <ericjinks@gmail.com>
2017-11-15 06:28:02 +1000

    Simplify <Card />

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 import { Title, Flex, BackgroundImage } from './common'
 import { color } from '../globalStyles'
 
+export default ({ to, title, image, brightness }) => (
+  <CardWrap to={to}>
+    <BackgroundImage
+      className='CardBackgroundImage'
+      image={image}
+      opacity={brightness}
+    />
+    <CardInner className='CardInner' alignCenter justifyStart>
+      <Title className='CardTitle'>
+        <div className='background' />
+        <span className='animate-translate'>{title}</span>
+      </Title>
+    </CardInner>
+  </CardWrap>
+)
+
 const transition = `
   transition: all .1s ease-out;
 `
-
 const CardWrap = styled(Link)`
   position: relative;
   display: block;
@@ -18,7 +33,7 @@ const CardWrap = styled(Link)`
 
   .CardBackgroundImage {
     filter: saturate(30%);
-    transition: filter .1s ease-out;
+    transition: filter 0.1s ease-out;
   }
 
   &:hover,
@@ -26,7 +41,6 @@ const CardWrap = styled(Link)`
     text-decoration: none;
   }
 `
-
 const CardInner = styled(Flex)`
   position: relative;
   background: ${color.black};
@@ -36,37 +50,3 @@ const CardInner = styled(Flex)`
   mix-blend-mode: lighten;
   ${transition};
 `
-
-export default class Card extends Component {
-  constructor (props) {
-    super(props)
-    this.handleMouseMove = this.handleMouseMove.bind(this)
-  }
-
-  componentDidMount () {
-
-  }
-
-  handleMouseMove (e) {
-    console.log(e)
-  }
-
-  render () {
-    const { to, title, image, brightness } = this.props
-    return (
-      <CardWrap to={to}>
-        <BackgroundImage
-          className='CardBackgroundImage'
-          image={image}
-          opacity={brightness}
-        />
-        <CardInner className='CardInner' alignCenter justifyStart>
-          <Title className='CardTitle'>
-            <div className='background' />
-            <span className='animate-translate'>{title}</span>
-          </Title>
-        </CardInner>
-      </CardWrap>
-    )
-  }
-}
