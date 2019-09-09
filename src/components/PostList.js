8bc2ef3914042442be89c25772537e0ffdcb7e0f
Eric Jinks <ericjinks@gmail.com>
2019-09-10 06:37:23 +1000

    Hide More Posts button on blog

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -101,16 +101,14 @@ export default ({ postCount }) => {
     'frontmatter.date'
   ).reverse()
 
-  if (postCount) {
-    posts = posts.slice(0, postCount)
-  }
+  const slicedPosts = postCount ? posts.slice(0, postCount) : posts
 
   return (
     <Section thin>
       <Container skinny>
         <TextContainer style={{ padding: '5rem 0' }}>
-          {posts &&
-            posts.map(post => (
+          {slicedPosts &&
+            slicedPosts.map(post => (
               <div style={{ margin: '0 auto 5rem auto' }} key={post.id}>
                 <StyledLink to={post.fields.slug}>
                   <h2>{post.frontmatter.title}</h2>
@@ -122,9 +120,11 @@ export default ({ postCount }) => {
                 </StyledLink>
               </div>
             ))}
-          <MoreButton to="/blog">
-            More Posts <ArrowRight style={{ height: '0.9em' }} />
-          </MoreButton>
+          {slicedPosts.length !== posts.length && (
+            <MoreButton to="/blog">
+              More Posts <ArrowRight style={{ height: '0.9em' }} />
+            </MoreButton>
+          )}
         </TextContainer>
       </Container>
     </Section>
