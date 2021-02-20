bf3f1690e4a2ccc1d34faa586bebf9472df85699
Eric Jinks <ericjinks@gmail.com>
2021-02-20 22:23:31 +1000

    Darken CanvasSketch

--- src/components/BackgroundSketch.js
+++ src/components/BackgroundSketch.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { Fixed } from './common'
 import sketch016 from '../sketches/sketch016'
 const CanvasSketch = React.lazy(() => import('../components/CanvasSketch'))
 
+const BackgroundStyled = styled(Fixed)`
+  [data-theme='dark'] & {
+    opacity: 0.8;
+  }
+`
+
 const BackgroundSketch = () => (
-  <Fixed>
+  <BackgroundStyled>
     <CanvasSketch sketch={sketch016} />
-  </Fixed>
+  </BackgroundStyled>
 )
 
 export default BackgroundSketch
