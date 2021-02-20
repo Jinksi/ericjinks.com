644e5a2e12b815aaf40f805e1c9e0a5bf3edec41
Eric Jinks <ericjinks@gmail.com>
2021-02-20 22:15:07 +1000

    Add Footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
+import Github from 'react-feather/dist/icons/github'
 
 import Wave from './Wave'
 import { Container } from './common'
 
 const Footer = styled.footer`
   margin-top: auto;
-  min-height: 50vh;
+  min-height: 250px;
 `
 
 const FooterInner = styled(Container)`
-  font-size: 1.4rem;
-  padding-top: 1rem;
-  text-align: right;
+  font-size: 0.9em;
+  padding: 10rem;
+  text-align: center;
+  color: var(--offWhite);
 `
+
+const Icon = styled.div`
+  width: 1em;
+  height: 1em;
+  vertical-align: middle;
+  margin-right: 0.2em;
+`
+const GithubIcon = Icon.withComponent(Github)
 
 export default () => (
   <Footer>
     <Wave flip />
-    <FooterInner></FooterInner>
+    <FooterInner>
+      <GithubIcon />{' '}
+      <a href="https://github.com/jinksi/ericjinks.com">
+        View source on GitHub
+      </a>
+    </FooterInner>
   </Footer>
 )
