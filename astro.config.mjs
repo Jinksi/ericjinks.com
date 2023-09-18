6088f8c01b23058e8dd94c6a405d25aefb515a5e
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-09-18 21:43:14 +1000

    Add netlify adapter for hybrid SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import react from '@astrojs/react';
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-import react from '@astrojs/react'
 export default defineConfig({
   site: 'https://ericjinks.com',
   integrations: [mdx(), sitemap(), react()],
@@ -12,7 +14,9 @@ export default defineConfig({
     syntaxHighlight: 'shiki',
     shikiConfig: {
       langs: ['js', 'html', 'css', 'r', 'jsx', 'typescript', 'tsx', 'docker'],
-      theme: 'one-dark-pro',
-    },
+      theme: 'one-dark-pro'
+    }
   },
-})
+  output: "hybrid",
+  adapter: netlify()
+});
