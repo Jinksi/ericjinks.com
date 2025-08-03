5d028710a82018bf1998d1e00c1ad3947e81c986
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2025-08-03 19:47:18 +1000

    Add astro-pagefind dep for site search capabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,18 @@ import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
+import pagefind from 'astro-pagefind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ericjinks.com',
+  build: {
+    format: 'file',
+  },
   integrations: [mdx(), sitemap(), react(), svelte(), tailwind({
     // Apply Tailwind only to React components and MDX files (hybrid approach)
     applyBaseStyles: false,
-  })],
+  }), pagefind()],
   trailingSlash: 'always',
   markdown: {
     syntaxHighlight: 'shiki',
